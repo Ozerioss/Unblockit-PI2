c4b20fa6f08be3b185eb551594ef8dea2a6deedb
Karim Kidiss <kkidiss@yahoo.fr>
2017-07-22 22:36:27 +0200

    Minor fixes

--- Ressources/js/app.js
+++ Ressources/js/app.js
@@ -1,4 +1,4 @@
-// Function pour animer le scroll down
+// Function to animate the scroll down
 $(function(){
 
 $('nav a, .down-button a').bind('click', function ()
@@ -14,13 +14,15 @@ $('nav a, .down-button a').bind('click', function ()
 
 
 // Fonction pour fix le footer au bottom de la page
- $(document).ready(function() {
+$(document).ready(function() {
 
-   var docHeight = $(window).height();
-   var footerHeight = $('#fixfooter').height();
-   var footerTop = $('#fixfooter').position().top + footerHeight;
+  var docHeight = $(window).height();
+  var footerHeight = $('#fixedFooter').height();
+  var offset = -9;
+
+  var footerTop = $('#fixedFooter').position().top + footerHeight;
 
    if (footerTop < docHeight) {
-    $('#fixfooter').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+    $('#fixedFooter').css('margin-top', offset + (docHeight - footerTop) + 'px');
    }
-  });
+});
